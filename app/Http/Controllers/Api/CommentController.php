02076febd6b02ced79b94f2955399f26848bbe32
someoneNameGrey <haozhe.yee@gmail.com>
2021-04-09 23:32:23 +0800

    includes post-comments object in comments filter

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -12,25 +12,55 @@ class CommentController extends Controller
     public function index(Request $request)
     {
         $client = new Client();
+        //GET posts
+        $postRes = $client->request('GET', static::postsEndpoint);
+        $posts = json_decode($postRes->getBody());
         //GET comments
         $commentRes = $client->request('GET', static::commentsEndpoint);
         $comments = json_decode($commentRes->getBody());
 
-        $commentArr = array();
+        $commentsArr = array();
+        foreach ($comments as $comment) {
+            if (!array_key_exists($comment->postId, $commentsArr)) {
+                $commentsArr[$comment->postId] = array();
+            } 
+            array_push($commentsArr[$comment->postId], $comment);
+        }
+
+        foreach ($posts as $post) {
+            $post->comments = $commentsArr[$post->id];
+        }
 
         if ($request->query('q')) {
+            $resultPosts = array();
             $terms = $request->query('q');
-            foreach ($comments as $comment) {
-                $objVar = get_object_vars($comment);
-                foreach ($objVar as $key => $value) {
-                    if (strpos($value, $terms) !== false )  {
-                        array_push($commentArr, $comment);
-                        break;
+
+            foreach ($posts as $post) {
+                $postObjVar = get_object_vars($post);
+                foreach ($postObjVar as $postKey => $value) {
+                    //excluding comments field in this search
+                    if ($postKey != 'comments') {
+                        if (strpos($value, $terms) !== false )  {
+                            array_push($resultPosts, $post);
+                            break;
+                        }
+                    }
+                    //if there's no matching words in post, then go for comments
+                    else {
+                        foreach ($post->comments as $comment) {
+                            $objVar = get_object_vars($comment);
+                            foreach ($objVar as $key => $value) {
+                                if (strpos($value, $terms) !== false )  {
+                                    array_push($resultPosts, $post);
+                                    break;
+                                }
+                            }
+                        }
                     }
                 }
             }
-            return $commentArr;
+            return $resultPosts;
         }
-        return $comments;
+        return $posts;
     }
 }
